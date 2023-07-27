d3ea23d79d2ac76c9c8151cbd718a44d686c8904
Artem Shestopalov <tema.rastaclaus@gmail.com>
2023-07-28 02:08:41 +0300

    update python lint settngs

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,6 +1,17 @@
-require("lint").linters.pylint = require("plugins/pylint")
+-- require("lint").linters.pylint = require("plugins/pylint")
+-- require("lint").linters.mypy = require("plugins/mypy")
+local mypy = require('lint').linters.mypy
+
+mypy.args = {
+    '--show-column-numbers',
+    '--show-error-codes',
+    '--hide-error-context',
+    '--no-color-output',
+    '--no-error-summary',
+    '--no-pretty',
+}
 
 require("lint").linters_by_ft = {
-    python = {"pycodestyle", "pylint", "mypy"},
+    python = {"ruff", "mypy", "pycodestyle"},
     yaml = {"yamllint"}
 }
