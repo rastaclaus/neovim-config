fafba69b751a2c6a0843655b17cc1b00e9a2625f
Artem Shestopalov <tema.rastaclaus@gmail.com>
2024-07-06 05:31:23 +0300

    add some useful plugins

--- init.lua
+++ init.lua
@@ -4,4 +4,3 @@ require('config/lazy')
 require('config/keymaps')
 require('config/autocmd')
 require('config/diagnostic')
-
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
                 ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python" },
                 sync_install = false,
                 highlight = { enable = true },
-                indent = { enable = true },  
+                indent = { enable = false },
             })
         end
     }
