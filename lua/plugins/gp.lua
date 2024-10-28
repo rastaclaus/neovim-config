da313bf32d3afdd702706c043a15481a27817f3d
Artem Shestopalov <tema.rastaclaus@gmail.com>
2024-10-28 21:14:38 +0300

    add gp.nvim

--- lua/plugins/gp.lua
+++ lua/plugins/gp.lua
@@ -10,7 +10,19 @@ return {
                     endpoint = "https://bothub.chat/api/v2/openai/v1/chat/completions"
                 }
             },
-            log_file = vim.fn.stdpath("log"):gsub("/$", "") .. "/gp.nvim.log"
+            log_file = vim.fn.stdpath("log"):gsub("/$", "") .. "/gp.nvim.log",
+            default_command_agent = "claude-3.5-sonnet",
+            default_chat_agent = "claude-3.5-sonnet",
+            agents = {
+                {
+                    provider = "openai",
+                    name = "claude-3.5-sonnet",
+                    chat = true,
+                    command = true,
+                    model = { model = "claude-3.5-sonnet", temperature = 0.8, top_p = 1 },
+                    system_prompt = require("gp.defaults").chat_system_prompt,
+                }
+            }
         }
         require("gp").setup(conf)
 
