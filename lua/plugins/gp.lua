3f77a5d6f648a731828c096c969c8b2e6d6c5877
Artem Shestopalov <tema.rastaclaus2016@ya.ru>
2025-03-07 14:46:23 +0300

    update ai settings

--- lua/plugins/gp.lua
+++ lua/plugins/gp.lua
@@ -4,43 +4,18 @@ return {
 		local ilp_prompt = "You are an advanced programming assistant specialized in Intermediate Literate Programming (ILP) methodology. "
 			.. "Your primary role is to help developers design, document, and clearly explain applications. Adhering to ILP, you consistently "
 			.. "provide detailed explanations and rationale alongside clearly structured and well-commented code snippets.\n\n"
-			.. "When generating Python code, you must follow strict type annotation guidelines based on the Python version specified by the user"
-			.. " (the default version of python is assumed to be 3.11 or higher):\n\n"
-			.. "- For Python 3.9 and above:\n"
-			.. "- Use built-in container types for annotations (list, dict, tuple, set).\n"
-			.. "  example:\n"
-			.. "  `def function_name(arg: int, data: list[str]) -> dict[str, int]:`\n\n"
-			.. "- For Python versions 3.10, 3.11, 3.12, and 3.13:\n"
-			.. "  Adopt modern type annotation syntax (PEP 604):\n"
-			.. "    Use `X | None` instead of `Optional[X]`\n"
-			.. "    Use `X | Y` instead of `Union[X, Y]`\n"
-			.. "  Example syntax for Python ≥3.10:\n"
-			.. "  def function_name(arg: int | None, data: list[str | int]) -> dict[str, int | float]:"
-			.. "In all generated code snippets, please additionally follow these best practices:\n\n"
-			.. "- Clearly explain each logical step using Intermediate Literate Programming style:\n"
-			.. "    - Provide descriptive paragraphs before or alongside code segments.\n"
-			.. "    - Focus on explaining the reasoning behind decisions, architecture, algorithms, data structures, and implementation details.\n"
+			.. "When generating Python code, you must follow strict type annotation guidelines"
+			.. "- In all generated code snippets, please additionally follow these best practices:\n\n"
 			.. "- Ensure code snippets are well-documented with concise yet meaningful inline comments.\n"
 			.. "- Maintain readability and clarity to facilitate understanding and maintainability.\n"
 			.. "- Use standard Python coding conventions (PEP8 compliance).\n\n"
 			.. "Always clarify assumptions and ask follow-up questions if the user's request is ambiguous or incomplete."
 
-		local code_prompt = "You are an expert programming assistant focused on Python. "
+		local code_prompt = "You are an senior programming assistant focused on Python. "
 			.. "Always: Write only code, outline comments and docstrings, do not provide any other explanations.; "
 			.. "follow PEP8/best practices (meaningful names, error handling, type hints where helpful). "
 			.. "Prioritize: 1) Solving the problem 2) Readability 3) Efficiency. "
-			.. "When generating Python code, you must follow strict type annotation guidelines based on the Python version specified by the user"
-			.. " (the default version of python is assumed to be 3.11 or higher):\n\n"
-			.. "- For Python 3.9 and above:\n"
-			.. "- Use built-in container types for annotations (list, dict, tuple, set).\n"
-			.. "  example:\n"
-			.. "  `def function_name(arg: int, data: list[str]) -> dict[str, int]:`\n\n"
-			.. "- For Python versions 3.10, 3.11, 3.12, and 3.13:\n"
-			.. "  Adopt modern type annotation syntax (PEP 604):\n"
-			.. "    Use `X | None` instead of `Optional[X]`\n"
-			.. "    Use `X | Y` instead of `Union[X, Y]`\n"
-			.. "  Example syntax for Python ≥3.10:\n"
-			.. "  def function_name(arg: int | None, data: list[str | int]) -> dict[str, int | float]:"
+			.. "When generating Python code, you must follow strict type annotation guidelines"
 		local translate_prompt = "You a good translator. translate text properly."
 
 		local home = os.getenv("HOME")
@@ -53,7 +28,7 @@ return {
 				},
 			},
 			log_file = vim.fn.stdpath("log"):gsub("/$", "") .. "/gp.nvim.log",
-			default_command_agent = "claude-3.7-sonnet",
+			default_command_agent = "qwen-2.5-coder-32b-instruct",
 			default_chat_agent = "claude-3.7-sonnet-ilp",
 			agents = {
 				{
@@ -79,6 +54,22 @@ return {
 					command = true,
 					model = { model = "llama-3.3-70b-instruct", temperature = 0.7, top_p = 1 },
 					system_prompt = translate_prompt,
+				},
+				{
+					provider = "openai",
+					name = "qwq-32b-preview",
+					chat = true,
+					command = true,
+					model = { model = "qwq-32b-preview", temperature = 0.7, top_p = 1 },
+					system_prompt = ilp_prompt,
+				},
+				{
+					provider = "openai",
+					name = "qwen-2.5-coder-32b-instruct",
+					chat = true,
+					command = true,
+					model = { model = "qwen-2.5-coder-32b-instruct", temperature = 0.7, top_p = 1 },
+					system_prompt = code_prompt,
 				},
 			},
 		}
