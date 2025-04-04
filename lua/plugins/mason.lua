0571268a914df356c09b7abe42774794b0e48d0b
Tema Rastaclaus <tema.rastaclaus@gmail.com>
2025-04-04 20:08:35 +0300

    update config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,60 +1,54 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-    },
-    config = function()
-        require("mason").setup({
-            ensure_installed = {
-                "basedpyright",
-                "ruff",
-            }
-        })
-        require("mason-lspconfig").setup({
-            ensure_installed = {},
-        })
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		require("mason").setup({})
+		require("mason-lspconfig").setup({
+			ensure_installed = {},
+		})
+		require("mason-lspconfig").setup_handlers({
+			-- The first entry (without a key) will be the default handler
+			-- and will be called for each installed server that doesn't have
+			-- a dedicated handler.
+			function(server_name) -- default handler (optional)
+				require("lspconfig")[server_name].setup({
+					on_attach = on_attach_handler,
+					flags = {
+						debounce_text_changes = 150,
+					},
+				})
+			end,
+			-- Next, you can provide a dedicated handler for specific servers.
+			["basedpyright"] = function()
+				local lspconfig = require("lspconfig")
 
-        require("mason-lspconfig").setup_handlers {
-            -- The first entry (without a key) will be the default handler
-            -- and will be called for each installed server that doesn't have
-            -- a dedicated handler.
-            function(server_name) -- default handler (optional)
-                require("lspconfig")[server_name].setup {
-                    on_attach = on_attach_handler,
-                    flags = {
-                        debounce_text_changes = 150,
-                    }
-                }
-            end,
-            -- Next, you can provide a dedicated handler for specific servers.
-            ["basedpyright"] = function()
-                local lspconfig = require("lspconfig")
+				lspconfig["basedpyright"].setup({
+					on_attach = on_attach_handler,
+					single_file_support = true,
+					settings = {
+						basedpyright = {
+							analysis = {
+								-- ignore = {'*'},
+							},
+						},
+					},
+				})
+			end,
+			["ruff"] = function()
+				local lspconfig = require("lspconfig")
 
-                lspconfig["basedpyright"].setup {
-                    on_attach = on_attach_handler,
-                    single_file_support = true,
-                    settings = {
-                        basedpyright = {
-                            analysis = {
-                                -- ignore = {'*'},
-                            }
-                        },
-                    }
-                }
-            end,
-            ["ruff"] = function()
-                local lspconfig = require("lspconfig")
-
-                lspconfig["ruff"].setup( {
-                    init_options = {
-                        settings = {
-                            configuration = "~/.ruff.toml",
-                            configurationPreference ="filesystemFirst",
-                        }
-                    }
-                })
-            end,
-        }
-    end,
+				lspconfig["ruff"].setup({
+					init_options = {
+						settings = {
+							configuration = "~/.ruff.toml",
+							configurationPreference = "filesystemFirst",
+						},
+					},
+				})
+			end,
+		})
+	end,
 }
