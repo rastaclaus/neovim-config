112b5d5cdea7fd15bf983b4f8d9187321c69faa2
Tema Rastaclaus <tema.rastaclaus@gmail.com>
2025-04-09 17:41:23 +0300

    add auto mcphub install

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -5,9 +5,9 @@ return {
 	},
 	-- comment the following line to ensure hub will be ready at the earliest
 	-- cmd = "MCPHub", -- lazy load by default
-	build = "npm install mcp-hub@latest", -- Installs required mcp-hub npm module
+	-- build = "npm install mcp-hub@latest", -- Installs required mcp-hub npm module
 	-- uncomment this if you don't want mcp-hub to be available globally or can't use -g
-	-- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+	build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
 	config = function()
 		require("mcphub").setup({
 			extensions = {
