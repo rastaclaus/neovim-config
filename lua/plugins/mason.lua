5499d56d69b96a8851cddd3add93e14db94da788
Tema Rastaclaus <tema.rastaclaus@gmail.com>
2025-04-09 17:37:01 +0300

    add auto lsp install

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,13 +2,24 @@ return {
 	"williamboman/mason.nvim",
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
 		"neovim/nvim-lspconfig",
 	},
 	config = function()
 		require("mason").setup({})
-		require("mason-lspconfig").setup({
-			ensure_installed = {},
-		})
+		require("mason-lspconfig").setup({})
+        require('mason-tool-installer').setup ({
+            ensure_installed = {
+                "lua-language-server",
+                "stylua",
+                "basedpyright",
+                "ruff",
+                "json-lsp",
+                "jq",
+                "yaml-language-server",
+                "yamlfix"
+            }
+        })
 		require("mason-lspconfig").setup_handlers({
 			-- The first entry (without a key) will be the default handler
 			-- and will be called for each installed server that doesn't have
