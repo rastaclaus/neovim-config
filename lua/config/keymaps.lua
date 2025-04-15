026364b93b93f20119f7fc15dd96ad40d694aca8
Tema Rastaclaus <tema.rastaclaus@gmail.com>
2025-04-16 01:43:49 +0300

    tabterm shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<leader><esc>", ":nohlsearch<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>cc", ":CodeCompanionChat Toggle<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>t", ":tab term<CR>", { noremap = true })
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
