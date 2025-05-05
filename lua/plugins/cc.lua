424036d004b8c20630f14bf3aef904864489b250
Tema Rastaclaus <tema.rastaclaus@gmail.com>
2025-05-05 21:00:16 +0300

    update cc config

--- lua/plugins/cc.lua
+++ lua/plugins/cc.lua
@@ -6,26 +6,10 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 	},
 	opts = {
-		display = {
-			diff = {
-				enabled = true,
-				close_chat_at = 220, -- Close an open chat buffer if the total columns of your display are less than...
-				layout = "vertical", -- vertical|horizontal split for default provider
-				opts = {
-					"internal",
-					"filler",
-					"closeoff",
-					"algorithm:histogram",
-					"indent-heuristic",
-					"followwrap",
-					"linematch:120",
-				},
-				provider = "default", -- default|mini_diff
-			},
-		},
 		adapters = {
 			opts = {
 				show_defaults = false,
+                show_model_choices = true,
 			},
 			["bothub"] = function()
 				return require("codecompanion.adapters").extend("openai_compatible", {
@@ -33,8 +17,9 @@ return {
 					formatted_name = "BotHub",
 					env = {
 						url = "https://bothub.chat/api/v2/openai",
-						chat_url = "/v1/chat/completions",
 						api_key = "cmd: cat ~/.config/openai_key",
+						chat_url = "/v1/chat/completions",
+						models_endpoint = "/v1/models",
 					},
 					schema = {
 						model = {
@@ -51,6 +36,9 @@ return {
 			inline = {
 				adapter = "bothub",
 			},
+		},
+		opts = {
+			log_level = "DEBUG",
 		},
 	},
 }
