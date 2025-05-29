fa21104c9a933e812bd2587e9d5eb88e259c4008
Tema Rastaclaus <tema.rastaclaus@gmail.com>
2025-05-29 15:40:50 +0300

    open term in vsplit, add cc settings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<leader><esc>", ":nohlsearch<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>cc", ":CodeCompanionChat Toggle<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>t", ":tab term<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>t", ":vsplit +term<CR>", { noremap = true })
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
 vim.keymap.set("v", "<leader>cc", ":CodeCompanion <CR>", { noremap = true })
--- lua/plugins/cc.lua
+++ lua/plugins/cc.lua
@@ -9,7 +9,7 @@ return {
 		adapters = {
 			opts = {
 				show_defaults = false,
-                show_model_choices = true,
+				show_model_choices = true,
 			},
 			["bothub"] = function()
 				return require("codecompanion.adapters").extend("openai_compatible", {
@@ -55,6 +55,18 @@ return {
 		},
 		opts = {
 			log_level = "DEBUG",
+		},
+		display = {
+			action_palette = {
+				width = 95,
+				height = 10,
+				prompt = "Prompt ", -- Prompt used for interactive LLM calls
+				provider = "snacks", -- Can be "default", "telescope", "fzf_lua", "mini_pick" or "snacks". If not specified, the plugin will autodetect installed providers.
+				opts = {
+					show_default_actions = true, -- Show the default actions in the action palette?
+					show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+				},
+			},
 		},
 	},
 }
