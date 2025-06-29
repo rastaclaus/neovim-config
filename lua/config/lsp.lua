a76bd49ab7ba78a81536eff60fab0352c8da6849
Artem Shestoaplov <tema.rastaclaus2016@yandex.ru>
2025-06-29 15:04:47 +0300

    configure blink

--- lua/config/lsp.lua
+++
@@ -1,9 +0,0 @@
--- Пример настройки LSP
-local lspconfig = require("lspconfig")
-
-lspconfig.lua_ls.setup({
-	on_attach = function(client, bufnr)
-		-- Включение встроенного автодополнения для клиента LSP
-		-- Другие настройки (ключи, форматирование и т.д.)
-	end,
-})
