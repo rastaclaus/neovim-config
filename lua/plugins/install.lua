dd1609e194d58b3d753bc10c8f143d120af773a5
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-08-07 03:47:33 +0300

    add gitsigns

--- lua/plugins/install.lua
+++ lua/plugins/install.lua
@@ -25,6 +25,7 @@ vim.pack.add({
 		 src = "https://github.com/Kurama622/llm.nvim",
 		 cmd = {"LLMSessionToggle", "LLMSelectedTextHandler", "LLMAppHandler"},
 	 },
+	 { src = "https://github.com/lewis6991/gitsigns.nvim" },
 })
 
 require("mason").setup()
