db20f597a00d94e8a3af79be23a2df80384e046e
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-08-15 14:18:02 +0300

    auto light/dark

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,27 +1,6 @@
-require("gruvbox-material").setup({
-	contrast = "hard",
-	background = { transparent = false },
-})
-
-require("transparent").setup({})
-
-require("darklight").setup({
-	mode="custom",
-	light_mode_callback = function()
-		vim.cmd("TransparentDisable")
-		vim.cmd("set bg=light")
-		vim.cmd("colorscheme bamboo")
-	end,
-	dark_mode_callback = function()
-		vim.cmd("set bg=dark")
-		vim.cmd("colorscheme bamboo")
-	end,
-})
-
 require("bamboo").setup({
-	toggle_style_key = "<leader>bs"
-})
-vim.keymap.set("n", "<leader>bd", "<cmd>DarkLightSwitch<CR>", {noremap=true, desc="Dark/Light switch"})
-vim.keymap.set("n", "<leader>bt", "<cmd>TransparentToggle<CR>", {noremap=true, desc="Transparency toggle"})
 
-vim.cmd("colorscheme bamboo")
+})
+require("transparent").setup({})
+vim.keymap.set("n", "<leader>bt", "<cmd>TransparentToggle<CR>", {noremap=true, desc="Transparency toggle"})
+vim.cmd.colorscheme("bamboo")
