66c2359ab8469b0ae7c3f4ebedec4ddfd61b0914
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-08-16 23:40:11 +0300

    use bamboo as colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,4 +2,5 @@ require("bamboo").setup({})
 require("gruvbox-material").setup({contrast="medium"})
 require("transparent").setup({})
 vim.keymap.set("n", "<leader>bt", "<cmd>TransparentToggle<CR>", {noremap=true, desc="Transparency toggle"})
-vim.cmd.colorscheme("gruvbox-material")
+vim.cmd.colorscheme("bamboo")
+
